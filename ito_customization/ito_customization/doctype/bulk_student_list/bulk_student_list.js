c75aa6f0f06c0042d7b28de20393abbd69a6038b
Finbyz <info@finbyz.tech>
2026-06-25 12:20:02 +0000

    Item and Customer fields

---
+++ ito_customization/ito_customization/doctype/bulk_student_list/bulk_student_list.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, FinByz Tech Pvt Ltd and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Bulk Student List", {
+// 	refresh(frm) {
+
+// 	},
+// });
