1d2259649db92a6becd90eed75feb48b11bb4a0f
Finbyz <info@finbyz.tech>
2026-06-26 12:54:54 +0000

    filter class

--- ito_customization/ito_customization/doctype/school_subject/school_subject.js
+++ ito_customization/ito_customization/doctype/school_subject/school_subject.js
@@ -1,8 +1,25 @@
-// Copyright (c) 2026, FinByz Tech Pvt Ltd and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("School Subject", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on('School Subject', {
+    onload: function(frm) {
+        // text_book & work_book → little_champ = 1 (checked)
+        frm.set_query('class', 'text_book', function() {
+            return { filters: { 'little_champ': 1 } };
+        });
+        
+        frm.set_query('class', 'work_book', function() {
+            return { filters: { 'little_champ': 1 } };
+        });
+        
+        // practice_workbook_110, prev_year_paper_160, student_guide_220 → little_champ = 0 (unchecked)
+        frm.set_query('class', 'practice_workbook_110', function() {
+            return { filters: { 'little_champ': 0 } };
+        });
+        
+        frm.set_query('class', 'prev_year_paper_160', function() {
+            return { filters: { 'little_champ': 0 } };
+        });
+        
+        frm.set_query('class', 'student_guide_220', function() {
+            return { filters: { 'little_champ': 0 } };
+        });
+    }
+});
