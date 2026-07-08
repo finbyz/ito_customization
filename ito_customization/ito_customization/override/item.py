a8871169b8cd68779ce893db76ded77727def2f0
Finbyz <info@finbyz.tech>
2026-07-08 07:27:48 +0000

    ps naming issue

--- ito_customization/ito_customization/override/item.py
+++ ito_customization/ito_customization/override/item.py
@@ -1,6 +1,6 @@
 import frappe
 from frappe import _
-from customer_portal.webshop_compat.doctype.override_doctype.item import WebshopItem as _Item
+from erpnext.stock.doctype.item.item import Item as _Item
 
 
 class CustomItem(_Item):
